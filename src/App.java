08d390f68446f8ec3b930e0de19c04e06dddd029
tahaabugra <tahabugracck@gmail.com>
2024-06-02 18:36:57 +0300

    application launch class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
-public class App {
+import javax.swing.*;
+
+public class App{
     public static void main(String[] args) {
-        
+        try {
+            new LoginScreen();
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(null, "Error" + exception);
+        }
     }
 }
