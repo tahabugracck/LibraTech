08c2e0326372406e11b42e8a6cfc3b71bc256fb0
tahaabugra <tahabugracck@gmail.com>
2024-06-02 18:39:01 +0300

    LibraTech user action

--- src/User.java
+++ src/User.java
@@ -1,2 +1,21 @@
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String IDname;
+    private String password;
+
+    public User(String IDname, String password) {
+        this.IDname = IDname;
+        this.password = password;
+    }
+
+    public String getIDname() {
+        return IDname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
 }
