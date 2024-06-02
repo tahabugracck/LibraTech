7812e3ef8a32805fb2a28a6a73f8e7ac7e2ce91b
tahaabugra <tahabugracck@gmail.com>
2024-06-02 18:39:06 +0300

    LibraTech user action

--- src/UserManager.java
+++ src/UserManager.java
@@ -1,2 +1,64 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
+
 public class UserManager {
+    private ArrayList<User> users;
+
+    public UserManager() {
+        loadUsersFromFile();
+    }
+
+    public void addUser(User user) {
+        if (!isUserIDNameTAken(user.getIDname())) {
+            users.add(user);
+            saveUsersToFile();
+        } else {
+            JOptionPane.showMessageDialog(null, "Username already taken.");
+        }
+    }
+
+
+    public boolean authenticateUser(String ID, String password) {
+        for (User user : users) {
+            if (user.getIDname().equals(ID) && user.getPassword().equals(password)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public boolean isUserIDNameTAken(String ID) {
+        for (User user : users) {
+            if (user.getIDname().equals(ID)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void saveUsersToFile() {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("users.dat"));
+            oos.writeObject(users);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error saving users: " + e.getMessage());
+        }
+    }
+
+
+    public void loadUsersFromFile() {
+        File file = new File("users.dat");
+        if (file.exists()) {
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+                users = (ArrayList<User>) ois.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(null, "Error loading users: " + e.getMessage());
+            }
+        } else {
+            users = new ArrayList<>();
+        }
+    }
 }
