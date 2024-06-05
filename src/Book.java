4ec96eee7efa4d978d5fc0f10ac3025fb9746cc4
tahaabugra <tahabugracck@gmail.com>
2024-06-05 22:17:36 +0300

    update class

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,30 @@
-public class Book {
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + author;
+    }
 }
