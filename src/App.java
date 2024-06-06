d1eb50cb7a18fa7f0447651069f27348f27a4ec3
tahaabugra <tahabugracck@gmail.com>
2024-06-06 18:23:13 +0300

    update class

--- src/App.java
+++ src/App.java
@@ -1,11 +1,11 @@
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
-public class App{
+public class App {
     public static void main(String[] args) {
         try {
             new LoginScreen();
-        }catch (Exception exception){
-            JOptionPane.showMessageDialog(null, "Error" + exception);
+        } catch (Exception exception) {
+            JOptionPane.showMessageDialog(null, "Error: " + exception.getMessage());
         }
     }
 }
