d7c5c5b95cc0e26ae9d85392612ab6021a14167a
tahaabugra <tahabugracck@gmail.com>
2024-06-06 18:23:22 +0300

    update class

--- src/ConfigReader.java
+++ src/ConfigReader.java
@@ -2,14 +2,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-class ConfigReader {
+public class ConfigReader {
     private static final String CONFIG_FILE_PATH = "config.properties";
 
     public static Properties readConfig() throws IOException {
         Properties properties = new Properties();
-        FileInputStream fileInputStream = new FileInputStream(CONFIG_FILE_PATH);
-        properties.load(fileInputStream);
-        fileInputStream.close();
+        try (FileInputStream fileInputStream = new FileInputStream(CONFIG_FILE_PATH)) {
+            properties.load(fileInputStream);
+        }
         return properties;
     }
 }
